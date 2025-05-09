25482f8502f494e3d1d62e85cd40e55fec340a0e
Brij Mohan Shah <prizan.shah@gmail.com>
2025-05-09 21:44:01 +0900

    node

--- app.js
+++ app.js
@@ -13,14 +13,3 @@ app.listen(5000,()=>{
     console.log("The port Succefully connected.")
 })
 
-
-
-
-
-echo "# Laptop-Backend" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/brijmohanhemanti/Laptop-Backend.git
-git push -u origin main
